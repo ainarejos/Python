73ae72c83dfcb3378c483e8171b3c818ff7c3af3
ainarejos <ainarejos2@nigul.cide.es>
2021-03-12 14:44:58 +0100

    Array's test added.

--- Test/main.py
+++ Test/main.py
@@ -1,5 +1,5 @@
 """
-Este proyecto es una recopilacion de pruebas, esta sse realizaron a medida
+Este proyecto es una recopilacion de pruebas, esta se realizaron a medida
 que se fueron aprendiendo nuevos conceptos del lenguaje python.
 
 Desarrollador: Adán Inarejos.
@@ -11,13 +11,26 @@ ID: PyCharm.
 #Creación de una clase.
 class HelloWorld:
 
-    #Variables/Atributos
+    #Variables/Atributos.
     a="Hello World"
+
+    #Matriz.
+    array = ["Carlos", "Joshua", "Ismael"]
+
+    #Añadir itema a raiz
+    array.append("Adan")
 
     #Metodo de una clase.
     def mostrar_pantalla(self):
         # Print Simple.
         print("Hello World!")
+
+        #Mostar items de la matriz.
+        print(self.array[1] + ", " + self.array[2])
+
+        #Bucle para mostrar todos los items de una matriz.
+        for x in self.array:
+            print(x)
 
 #Finalización de la clase.
 
