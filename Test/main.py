5dd155ba0d5ddbeb96f5f90b94f6d6fc66c377cf
ainarejos <ainarejos2@nigul.cide.es>
2021-03-12 14:12:55 +0100

    Inital Push.

---
+++ Test/main.py
@@ -0,0 +1,26 @@
+"""
+Este proyecto es una recopilacion de pruebas, esta sse realizaron a medida
+que se fueron aprendiendo nuevos conceptos del lenguaje python.
+
+Desarrollador: Adán Inarejos.
+Fecha Inicio: 12/03/2021.
+Fecha Final: --/--/--.
+ID: PyCharm.
+"""
+
+#Creación de una clase.
+class HelloWorld:
+
+    #Variables/Atributos
+    a="Hello World"
+
+    #Metodo de una clase.
+    def mostrar_pantalla(self):
+        # Print Simple.
+        print("Hello World!")
+
+#Finalización de la clase.
+
+#Intanciar una clase y utilizar un metodo.
+x = HelloWorld
+x.mostrar_pantalla(x)
